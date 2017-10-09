bab0dac32415a055054726b0797b1ab0f719f260
Ankit Goyal <ak.goyal1987@gmail.com>
2017-10-10 01:08:07 +0530

    commiting code to send email using nodemailer

---
+++ index.js
@@ -0,0 +1,26 @@
+var nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+var transporter = nodemailer.createTransport('smtps://ak.goyal1987%40gmail.com:@Akgoyal87@smtp.gmail.com');
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: '"Fred Foo ?" <foo@blurdybloop.com>', // sender address
+    to: 'ak.goyal1987@gmail.com', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world ?', // plaintext body
+    attachments : [{
+        filename: 'package.json',
+        path: 'E:\\Examples\\nodemailer-example\\package.json',
+        contentType: 'application/json'
+    }],
+    html: '<b>Hello world ?</b>' // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+});
