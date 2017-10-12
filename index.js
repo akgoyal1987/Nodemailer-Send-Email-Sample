d3f6c294a37d162b7f4fefc37d75ac9b701e1105
Ankit Goyal <ak.goyal1987@gmail.com>
2017-10-12 23:26:02 +0530

    added path to package.json file

--- index.js
+++ index.js
@@ -2,27 +2,22 @@ var nodemailer = require('nodemailer');
 var directTransport = require('nodemailer-direct-transport');
 // create reusable transporter object using the default SMTP transport
 var transporter = nodemailer.createTransport(directTransport({}));
-
+var path = require('path');
 // setup e-mail data with unicode symbols
 var mailOptions = {
-    from: '"Fred Foo ?" <foo@blurdybloop.com>', // sender address
-    to: 'ak.goyal1987@gmail.com, libertytrustgroupllc@gmail.com', // list of receivers
-    subject: 'Hello ✔', // Subject line
-    text: 'Hello world ?', // plaintext body
-    // attachments : [{
-    //     filename: 'package.json',
-    //     path: 'E:\\Examples\\nodemailer-example\\package.json',
-    //     contentType: 'application/json'
-    // }],
-    html: '<b>Hello world ?</b>' // html body
-};
-
-transporter.sendMail({
     from: 'ak.goyal1987@testmail.com',
     to: 'ak.goyal1987@gmail.com, libertytrustgroupllc@gmail.com',
     subject: 'testing email',
     html: `<h1>hi, this is testing email sent without any SMPT server authenticaiton. message is sent by direct transport from server</h1>`
-}, function (error, result) {
+    text: 'Hello world ?', // plaintext body
+    attachments : [{
+        filename: 'package.json',
+        path: path.resolve(__dirname, 'package.json'),
+        contentType: 'application/json'
+    }],
+};
+
+transporter.sendMail(mailOptions, function (error, result) {
     if(error){
         console.log(error);
     }else{
